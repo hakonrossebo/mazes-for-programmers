0f4c2e230b3def9f3a8049effc7e818de70f1ca0
Håkon Rossebø <hakonrossebo@hotmail.com>
2019-12-30 08:35:37 +0100

    Grid class

--- src/oop/MazeGrid/MazeGrid.fs
+++ src/oop/MazeGrid/MazeGrid.fs
@@ -2,20 +2,20 @@ namespace MazeGrid
 open System.Collections.Generic
 open System.Linq
 
-module Utils =
-    let inline isNull (x:^T when ^T : not struct) = obj.ReferenceEquals (x, null)
+// module Utils =
+//     let inline isNull (x:^T when ^T : not struct) = obj.ReferenceEquals (x, null)
 
 [<AllowNullLiteral>]
-type Cell (row:int, column:int, links: Dictionary<Cell, bool>) =
-    let mutable links = links
+type Cell (row:int, column:int) =
+    let mutable links = new Dictionary<Cell, bool>()
     member this.Row = row
     member this.Column = column
-    // member this.links = links
 
     member val  North : Cell = null with get, set
     member val  South : Cell = null with get, set
     member val  West : Cell = null with get, set
     member val  East : Cell = null with get, set
+
 
     member this.Neighbors 
         with get() = 
@@ -50,9 +50,62 @@ type Cell (row:int, column:int, links: Dictionary<Cell, bool>) =
             links.ContainsKey(cell)
 
 type Grid(rows: int, columns:int) =
+    let rnd = System.Random()
     member this.Rows = rows
     member this.Columns = columns
     member this.Size = rows * columns
+    member val Cells : Cell [,] = null with get, set //private set?
 
-    member this.Cells : Cell [][]
+    member this.PrepareGrid() =
+        this.Cells <- Array2D.init rows columns (fun x y ->  Cell(x, y))
+
+    member this.ConfigureCells() =
+        this.Cells |> Array2D.iter (fun cell -> 
+                let row = cell.Row
+                let col = cell.Column
+                if row - 1 >= 0 then
+                    cell.North <- this.Cells.[row-1, col]
+                if row + 1 < this.Rows then
+                    cell.South <- this.Cells.[row+1, col]
+                if col - 1 >= 0 then
+                    cell.West <- this.Cells.[row, col - 1]
+                if col + 1  < this.Columns then
+                    cell.East <- this.Cells.[row, col + 1]
+            )
+
+    member this.GetCell(row, column) =
+        this.Cells.[row, column]
+
+    member this.RandomCell() =
+        let row = rnd.Next(this.Rows)
+        let column = rnd.Next(this.Columns)
+        this.Cells.[row, column]
+
+
+    member this.EachRow() =
+        seq {
+            for row in [0..this.Rows - 1] do
+                    yield this.Cells.[row,*]
+        }
+
+    member this.EachCell() =
+        seq {
+            for row in [0..this.Rows - 1] do
+                for col in [0..this.Columns - 1] do
+                    yield this.Cells.[row,col]
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/oop/Program.fs
+++ src/oop/Program.fs
@@ -1,9 +1,12 @@
 ﻿
 open System
+open MazeGrid
 
 [<EntryPoint>]
 let main argv =
     printfn "Maze OOP version in F#"
+    let g = Grid(10,10)
+    printfn "Grid created"
     0 // return an integer exit code
 
 
